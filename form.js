094905f073b8bfb41794d0ed86f22d4b87efc07f
azizz112 <159426415+azizz112@users.noreply.github.com>
2024-02-08 10:30:14 +0100

    Add files via upload

---
+++ form.js
@@ -0,0 +1,8 @@
+/*alert("il y a un script javascript dans cette page!!");
+let age=10;
+age=20;
+alert(age)
+let name=prompt("yoo gimme ur name shawty");
+alert(name)
+console.log('bjr');
+document.write("salut")*/
